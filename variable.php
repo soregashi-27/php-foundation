1aaa8b440a03fd58ac19ca6c9f8c9d72ef205f1a
soregashi-27 <tomo.soul.15@gmail.com>
2022-03-13 19:08:16 +0900

    変数の追加

--- variable.php
+++ variable.php
@@ -1,4 +1,14 @@
 <?php
     $jojo = "Joseph Jorstar";
+    $jonathan = "ジョナサン";
+    $joestar = "ジョースター";
     echo $jojo;
+    echo '<br>';
+    echo $jonathan;
+    echo '<br>';
+    echo $joestar;
+    echo '<br>';
+    $character = $joestar;
+    echo '<br>';
+    echo $character;
 ?>
