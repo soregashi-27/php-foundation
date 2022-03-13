a148f3d90cd573ee628d3b10539c3b37e7963a1b
soregashi-27 <tomo.soul.15@gmail.com>
2022-03-13 19:42:38 +0900

    intの復習

---
+++ int.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta
+    http-equiv="X-UA-Compatible"
+    content="IE=edge"
+  >
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0"
+  >
+  <title>php-foundation</title>
+</head>
+
+<body>
+  <?php
+    // $var = "Yo";
+    // echo $var;
+
+    // echo 10 * 4 + 5 - 2;
+    $x = 10 * 4 + 5 - 1;
+    echo $x;
+     ?>
+
+</body>
+
+</html>
