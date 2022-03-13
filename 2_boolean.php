cb8efca54a7c6f2a4bda0a02255adbd6c542e8d5
soregashi-27 <tomo.soul.15@gmail.com>
2022-03-13 21:22:47 +0900

    boolean

---
+++ 2_boolean.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta
+    http-equiv="X-UA-Compatible"
+    content="IE=edge"
+  >
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0"
+  >
+  <title>php-foundation</title>
+</head>
+
+<body>
+  <?php
+        // true or false
+        $x = TRUE && FALSE;
+        $y = TRUE and FALSE;
+        $z = ! TRUE; //FALSE
+     ?>
+
+</body>
+
+</html>
