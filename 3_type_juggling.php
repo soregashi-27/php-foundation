93fe204ed707d7b0daa6e37078b1d86904b8b0b3
soregashi-27 <tomo.soul.15@gmail.com>
2022-03-13 21:40:28 +0900

    juggling

---
+++ 3_type_juggling.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta
+    http-equiv="X-UA-Compatible"
+    content="IE=edge"
+  >
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0"
+  >
+  <title>php-foundation</title>
+</head>
+
+<body>
+  <?php
+    //scara typeはString, Int, Float（少数）, Boolean（True or false, 論理値とも言う）
+    //NULL型：慣例的には大文字で書くことになっているので注意。
+    //NULLは何もできない特殊な型
+    $x = NULL;
+
+
+     ?>
+
+</body>
+
+</html>
