270708d2213b335aee8bf89b1252b5e27fa44fa1
soregashi-27 <tomo.soul.15@gmail.com>
2022-03-15 00:58:49 +0900

    比較演算

--- 5_if.php
+++ 5_if.php
@@ -22,6 +22,25 @@
     }
     echo 'except function';
 
+    if (TRUE || FALSE ) {
+        $x = 5 + 3;
+        echo $x;
+         echo '<br/>';
+    }
+    echo '論理和の本質は原則true. 例外としてfalse';
+
+    if (TRUE && FALSE ) {
+        $b = 5 + 3;
+        echo $b;
+         echo '<br/>';
+    }
+    echo '論理積の本質は原則false. 2つの条件がそろったらTrue';
+
+    //比較演算は正しい時にTrue、間違ってるときにfalse。
+    //sample
+    var_dump(1 < 2);
+    echo '<br/>';
+    var_dump(2 < 3);
      ?>
 
 </body>
