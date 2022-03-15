18213695682edadc3159e145356323c83365d61c
soregashi-27 <tomo.soul.15@gmail.com>
2022-03-16 00:05:56 +0900

    if_else

---
+++ 7_if_else.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta
+    http-equiv="X-UA-Compatible"
+    content="IE=edge"
+  >
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0"
+  >
+  <title>Document</title>
+</head>
+
+
+<body>
+  <?php
+    //if_elseは2つの条件がお互いに排他的、かつ網羅的なときにつかえる
+    ?>
+
+</body>
+
+</html>
