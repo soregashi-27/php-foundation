71582f56d70e21597922045f4c725c232e57e368
soregashi-27 <tomo.soul.15@gmail.com>
2022-03-15 23:56:31 +0900

    暗黙の型変換

---
+++ 6_if_part2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta
+    http-equiv="X-UA-Compatible"
+    content="IE=edge"
+  >
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0"
+  >
+  <title>Document</title>
+</head>
+
+<body>
+
+  <?php
+        //比較演算は2つの数からbool型の値を作る演算
+        if (3 < 5) {
+            echo 'trueやで';
+        }
+
+        //暗黙の型変換
+        if ('文字列') {
+            echo 'これもtrueやで';
+        }
+    ?>
+</body>
+
+</html>
